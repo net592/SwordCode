793af983a76e81c8d53735e7b9bbe55da3d8b852
xuzeming6 <xuzeming6@jd.com>
2021-03-08 08:59:31 +0800

    fix(更新字符串)

---
+++ LetCode初级算法/02字符串/05-验证回文串.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 直接切割反转字符串
+        sgood = "".join(ch.lower() for ch in s if ch.isalnum())
+        return sgood == sgood[::-1]
+        
+        # 过滤符号空格
+        sgood = "".join([i.lower() for i in s if i.isalnum()])
+        left, right = 0, len(sgood) -1
+        while left < right:
+            if sgood[left] == sgood[right]:
+                left +=1
+                right -=1
+            else:
+                return False
+        return True
+
+if __name__ == "__main__":
+    s=Solution()
+    print(s.isPalindrome("aabaa"))
