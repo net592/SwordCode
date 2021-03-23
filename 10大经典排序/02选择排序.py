9760d46259fef59044681166477abc18ecdefd76
xuzeming6 <xuzeming6@jd.com>
2021-03-23 19:35:50 +0800

    fix(20210323)

--- 10大经典排序/02选择排序.py
+++ 10大经典排序/02选择排序.py
@@ -1,18 +1,17 @@
 """
 选择排序
 
-选择排序是一种简单直观的排序算法，无论什么数据进去都是 O(n²) 的时间复杂度。所以用到它的时候，数据规模越小越好。唯一的好处可能就是不占用额外的内存空间了吧。
+选择排序是一种简单直观的排序算法，无论什么数据进去都是 O(n²) 的时间复杂度。
+所以用到它的时候，数据规模越小越好。唯一的好处可能就是不占用额外的内存空间了吧。
 
 1. 算法步骤
 
-首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
-
-再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
-
-重复第二步，直到所有元素均排序完毕。
+#1 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置
+#2 再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+#3 重复第二步，直到所有元素均排序完毕。
 """
 
-arr = [5, 1, 10, 2, 6]
+arr = [5, 11, 4, 2, 3]
 
 def selectionSort(arr):
     for i in range(len(arr) - 1):  # 选择排序次数
@@ -28,25 +27,6 @@ def selectionSort(arr):
             arr[i], arr[minIndex] = arr[minIndex], arr[i]
     return arr
 
-
-
-
-
-
-
-
-
-def selectionSort(arr):
-    for i in range(len(arr)-1):  # 选择排序次数
-        minIndex = i
-        for j in range(i +1, len(arr)):  # 比较后续元素
-            print(i+1, len(arr))
-            if arr[j] < arr[minIndex]:
-                minIndex = j
-        # 如果 i 不是最小数，置换元素
-        if i != minIndex:
-            arr[i], arr[minIndex] = arr[minIndex], arr[i]
-    return arr
 
 
 if __name__ == '__main__':
---
+++ LetCode初级算法/07设计问题/01打乱数组.py
@@ -0,0 +1,24 @@
+class Solution:
+    def __init__(self, nums):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+        # 解法1 暴力穷举，空间复杂度2n 
+        # aux = copy.deepcopy(self.array)
+
+        # for idx in range(len(self.array)):
+        #     remove_idx = random.randrange(len(aux))
+        #     print(idx, aux)
+        #     self.array[idx] = aux.pop(remove_idx)
+
+        # 解法2 空间置换
+        for i in range(len(self.array)):
+            swap_idx = random.randrange(i, len(self.array))
+            self.array[i], self.array[swap_idx] = self.array[swap_idx], self.array[i]
+        return self.array
